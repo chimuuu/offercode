edda1f4cb5411e422bea40d078edf43c85abd8a4
chimuuu <w409048772@163.com>
2017-08-23 09:38:21 +0800

    二叉树的四种遍历方式：前序，中序，后序及层序

--- tree_travel.py
+++ tree_travel.py
@@ -3,9 +3,16 @@
 题目描述
 操作给定的二叉树，输出二叉树的三种遍历结果
 
-前序遍历：根左右 8 6 5 7 10 9 11    第一位是根节点
-中序遍历：左根右 5 6 7 8 9 10 11    中间一位是根节点
-后续遍历：左右根 5 7 6 9 10 10 8    最后一位是根节点
+前序遍历：根左右 8 6 5 7 10 9 11         第一位是根节点
+中序遍历：左根右 5 6 7 8 9 10 11         中间一位是根节点
+后续遍历：左右根 5 7 6 9 10 10 8         最后一位是根节点
+
+
+层序遍历与上述三种略有不同，要采用队列的方式进行遍历，而不再是递归
+层序遍历：上到下，左到右 8 6 10 5 7 9 11  第一位是根节点
+
+方法 1. 根节点入队列
+     2. while 判断队列不为空，则弹出第一个结点，访问，并将该结点的左右子孩子入队
 
 输入描述:
 
@@ -17,6 +24,9 @@
     	5   7 9  11
 
 '''
+import Queue
+
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -53,6 +63,22 @@ class Solution(object):
         self.PostOrderTraverse(root.right)
         print root.val
 
+    # 层序遍历：自上而下，同一层自左向右
+    def LevelOrderTraverse(self, root):
+        # 构造队列
+        treequeue = Queue.Queue()
+
+        if not root:
+            return
+
+        treequeue.put(root)
+        while treequeue:
+            root = treequeue.get()
+            print root.val
+            if root.left:
+                treequeue.put(root.left)
+            if root.right:
+                treequeue.put(root.right)
 
 
 pRoot1 = TreeNode(8)
@@ -78,4 +104,5 @@ if __name__ == '__main__':
     s.inOrderTraverse(pRoot1)
     print '<--------------后序遍历-------------->'
     s.PostOrderTraverse(pRoot1)
-
+    print '<--------------层序遍历-------------->'
+    s.LevelOrderTraverse(pRoot1)
